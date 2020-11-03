4fa0dd21b929ce55d92bfe44c92cb11ae6bbd314
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-11-03 17:07:04 +0100

    update test imports

--- tests/test_pyfar.py
+++ tests/test_pyfar.py
@@ -1,9 +1,4 @@
-import numpy as np
-import numpy.testing as npt     # noqa
-from pytest import raises
-
-
-def test_import_haiopy():
+def test_import_pyfar():
     try:
         import pyfar           # noqa
     except ImportError:
