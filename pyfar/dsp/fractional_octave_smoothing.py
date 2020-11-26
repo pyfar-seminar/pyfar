434eefb3bf194099016a4a40c5e87614d7330948
Linus Niederberger <niederberger@campus.tu-berlin.de>
2020-11-26 19:10:17 +0100

    Methode integration_limits hinzugefügt. Konstruktor & Docs angefangen.

---
+++ pyfar/dsp/fractional_octave_smoothing.py
@@ -0,0 +1,73 @@
+import numpy as np
+from pyfar import Signal
+
+
+class FractionalSmoothing:
+
+    def __init__(
+            self,
+            sampling_rate,
+            smoothing_width,
+            win_type='rectangular'):
+        """__init__ [summary]
+
+        :param sampling_rate: Sampling frequency of signal
+        :type sampling_rate: float, int
+        :param smoothing_width: Width of smoothing window relative to an octave
+        :type smoothing_width: float, int
+        :param win_type: Type of window applied to smooth,
+         defaults to 'rectangular'
+        :type win_type: str, optional
+        :raises ValueError: Invalid data type of sampling_rate.
+        :raises ValueError: Invalid data type of smoothing_width.
+        :raises ValueError: Invalid window type.
+        """
+        if isinstance(sampling_rate,  (float, int)) is True:
+            self._sampling_rate = sampling_rate
+        else:
+            raise ValueError("Invalid data type of sampling rate (int/float)")
+
+        if isinstance(smoothing_width, (float, int)) is True:
+            self._smoothing_width = smoothing_width
+        else:
+            raise ValueError("Invalid data type of window width (int/float)")
+
+        self._VALID_WINDOW_TYPE = ["rectangular"]
+        if (win_type in self._VALID_WIN_TYPE) is True:
+            self._win_type = win_type
+        else:
+            raise ValueError("Not a valid window type ('rectangular')")
+
+    def integration_limits(self):
+        """integration_limits Computes integration limits for each frequency bin
+        k.
+        TODO
+        """
+        # Lower and upper cutoff frequencies for each bin k:
+        cutoff_low = self._disc_freq_bins*2**(-self._smoothing_width/2)
+        cutoff_up = self._disc_freq_bins*2**(self._smoothing_width/2)
+        # Range of frequency bins within upper and lower cutoff frequency:
+        freq_bins = np.array([
+                        np.arange(np.floor(k_c_low), np.ceil(k_c_up))
+                        for k_c_low, k_c_up in zip(cutoff_low, cutoff_up)])
+        # Replace first entry by lower cutoff frequency bin k'
+        # and substract 0.5 from other entries:
+        freq_bins_low = np.array([
+                        np.concatenate((np.array([k_L]), k[1:]-.5))
+                        for k_L, k in zip(cutoff_low, freq_bins)])
+        # Replace last entry by upper cutoff frequency bin k'
+        # and add 0.5 to other entries:
+        freq_bins_up = np.array([
+                        np.concatenate((k[:-1]+.5, np.array([k_U])))
+                        for k, k_U in zip(freq_bins, cutoff_up)])
+        # Divide by k and calc log:
+        freq_bins_low /= self._disc_freq_bins
+        freq_bins_up /= self._disc_freq_bins
+        Phi_l = np.array([np.log2(i) for i in freq_bins_low])
+        Phi_u = np.array([np.log2(i) for i in freq_bins_up])
+        self._Phi = Phi = np.array([Phi_l, Phi_u])
+
+
+    # def calc_weights(self):
+
+    # def apply(self, signal):
