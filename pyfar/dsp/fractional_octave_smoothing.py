ef2a0972a1e89e3378e8fb1d0707c8eb1686a9e4
Linus Niederberger <niederberger@campus.tu-berlin.de>
2020-12-02 22:48:03 +0100

    Added some docu

--- pyfar/dsp/fractional_octave_smoothing.py
+++ pyfar/dsp/fractional_octave_smoothing.py
@@ -9,7 +9,7 @@ class FractionalSmoothing:
             sampling_rate,
             smoothing_width,
             win_type='rectangular'):
-        """__init__ [summary]
+        """__init__ Initiate FractionalSmoothing object.
 
         :param sampling_rate: Sampling frequency of signal
         :type sampling_rate: float, int
@@ -40,8 +40,12 @@ class FractionalSmoothing:
 
     def integration_limits(self):
         """integration_limits Computes integration limits for each frequency bin
-        k.
-        TODO
+        k. For each k two arrays are computed, one containing the upper, one
+        containing the lower integration limits.
+        The k arrays of upper and lower limits are stored in two arrays for
+        upper and lower limits. Finally, the log2 on all elements is computed
+        and the two arrays are combined to one. The array 'Phi' contains all
+        integration limits and is stored in the FractionalSmoothing object.
         """
         # Lower and upper cutoff frequencies for each bin k:
         cutoff_low = self._disc_freq_bins*2**(-self._smoothing_width/2)
@@ -52,22 +56,26 @@ class FractionalSmoothing:
                         for k_c_low, k_c_up in zip(cutoff_low, cutoff_up)])
         # Replace first entry by lower cutoff frequency bin k'
         # and substract 0.5 from other entries:
-        freq_bins_low = np.array([
+        disc_freq_bins_low = np.array([
                         np.concatenate((np.array([k_L]), k[1:]-.5))
                         for k_L, k in zip(cutoff_low, freq_bins)])
         # Replace last entry by upper cutoff frequency bin k'
         # and add 0.5 to other entries:
-        freq_bins_up = np.array([
+        disc_freq_bins_up = np.array([
                         np.concatenate((k[:-1]+.5, np.array([k_U])))
                         for k, k_U in zip(freq_bins, cutoff_up)])
         # Divide by k and calc log:
-        freq_bins_low /= self._disc_freq_bins
-        freq_bins_up /= self._disc_freq_bins
-        Phi_l = np.array([np.log2(i) for i in freq_bins_low])
-        Phi_u = np.array([np.log2(i) for i in freq_bins_up])
-        self._Phi = Phi = np.array([Phi_l, Phi_u])
+        disc_freq_bins_low /= self._disc_freq_bins
+        disc_freq_bins_up /= self._disc_freq_bins
+        Phi_l = np.array([np.log2(i) for i in disc_freq_bins_low])
+        Phi_u = np.array([np.log2(i) for i in disc_freq_bins_up])
+        self._Phi = np.array([Phi_l, Phi_u])
 
-
-    # def calc_weights(self):
+    def calc_weights(self):
+        if (self._win_type == "rectangular") is True:
+            # TODO Computation: Upper - Lower / Delta
+            self._weights = np.empty(
+                [len(self._disc_freq_bins), len(self._disc_freq_bins)])
 
     # def apply(self, signal):
+        
