1c511e5625dab7f937205e3d4a49c6f4567f727b
Linus Niederberger <niederberger@campus.tu-berlin.de>
2020-12-22 11:18:20 +0100

    Updated To-Do list Removed old integration limit test

--- pyfar/dsp/TODOs.md
+++ pyfar/dsp/TODOs.md
@@ -1,27 +1,27 @@
 # TODOs: Fractional Octave Smoothing Modul
 
 ## Grundstruktur für FractionalOctaveSmoothing Objekt:
-* [ ] Methoden definieren (init, calc_integration_limits, calc_weights, apply, 
+* [x] Methoden definieren (init, calc_integration_limits, calc_weights, apply, 
     smooth, ...)
-* [ ] Attribute definieren (Fensterbreite, Abtastfrequenz, Frequenzen an der 
-    geglättet wird, Integrationsgrenzen(?), Gewichte)
+* [x] Attribute definieren (Fensterbreite, Abtastfrequenz, Frequenzen an der geglättet wird, Integrationsgrenzen(?), Gewichte)
 * [ ] Get-& Set-Methoden definieren
 * [ ] Datentypen für Gewichte und Integrationsgrenzen festlegen
-* [ ] 
+* [ ] Methoden zum Smoothing anhand Signal und HRTF erstellen
 
 ## Signalverarbeitung
-* [ ] Paddingmethode für Smoothing am Rand des Spektrums überlegen
+* [x] Paddingmethode für Smoothing am Rand des Spektrums überlegen
 * [ ] Festlegen, wann Integrationsgrenzen und Gewichte erstmalig berechnet 
     werden, wann neue Berechnung nötig ist
 
 
 ## Testing:
-* [ ] Error Codes testen
-* [ ] Berechnung der Integrationsgrenzen mit analytischer Lösung vergleichen
-* [ ] Berechnung der Gewichte mt analytischer Lösung vergeleichen (Dirac?)
-* [ ] Get- & Set-Methoden testen
+* [x] Ctors und Error Exceptions testen
 * [ ] Verschiedene Konstruktormethoden testen 
+* [ ] Berechnung der Integrationsgrenzen 
+* [ ] Berechnung der Gewichte mit analytischer Lösung vergeleichen (Dirac?)
+* [ ] Get- & Set-Methoden testen
 * [ ] Smoothing eines mehrkanaliges Eingangssignal testen
+* [ ] Smoothing einer mehrkanaliger HRTF testen
 * [ ] Alle Methoden getestet?
 
 ## Doku:
--- tests/test_fractional_octave_smoothing.py
+++ tests/test_fractional_octave_smoothing.py
@@ -46,68 +46,6 @@ def test_init_exceptions():
     assert str(error.value) == "Invalid data type of window width (int/float)."
 
 
-def DISABLED_test_calc_integration_limits():
-    channel_number = 1
-    signal_length = 5      # Signal length in freq domain
-    data = np.empty((channel_number, signal_length), dtype=np.complex)
-    win_width = 1
-    win_type = 'rectangular'
-    # Create smoothing object
-    smoother = fs.FractionalSmoothing(data, win_width, win_type)
-    # Compute integration limits
-    smoother.calc_integration_limits()
-    # Get integration_limits
-    limits = smoother._limits
-
-    # Check limits:
-    # Freq bin zero:
-    assert limits[0, :, :].all() == 0.0
-    # Freq bin greater then zero:
-    for k in range(1, limits.shape[0]):
-        k_i = np.arange(limits.shape[2])
-        # Compute cutoff bin of rectangular window:
-        expected_lower_cutoff = k*2**(-win_width/2)
-        expected_upper_cutoff = k*2**(win_width/2)
-        # Indices of cutoff bins in array:
-        # Last element that is smaller then lower cutoff
-        idx_lower_cutoff = np.argmin((k_i - .5) < expected_lower_cutoff) - 1
-        # If cutoff is larger than max signal size
-        # -> last element is cutoff idx
-        if expected_upper_cutoff > (k_i[-1]+.5):
-            idx_upper_cutoff = k_i[-1]
-        # Otherwise: first element that is larger then upper cutoff:
-        else:
-            idx_upper_cutoff = np.argmax(expected_upper_cutoff < (k_i + .5))
-
-        # Limits at cutoff:
-        expected_lower_limit = np.log2(expected_lower_cutoff/k)
-        expected_upper_limit = np.log2(expected_upper_cutoff/k)
-
-        # Limit at cutoff must equal log2(cutoff/idx_cutoff):
-        # Upper limit in '0'
-        assert limits[k, 0, int(idx_upper_cutoff)] == approx(
-            expected_upper_limit)
-        # Lower limit in '1'
-        assert limits[k, 1, int(idx_lower_cutoff)] == approx(
-            expected_lower_limit)
-
-        # All limits smaller then cutoff must be zero:
-        for k_i in range(int(idx_lower_cutoff)):
-            assert limits[k, :, k_i] == approx(np.zeros(2))
-
-        # All limits greater then cutoff must be zero:
-        for k_i in range(int(idx_upper_cutoff)+1, limits.shape[2]):
-            assert limits[k, :, k_i] == approx(np.zeros(2))
-
-        # All limits in between:
-        for k_i in range(int(idx_lower_cutoff), int(idx_upper_cutoff)):
-            # Upper limit from idx_low to idx_upper - 1
-            assert limits[k, 0, k_i] == np.log2((k_i+.5)/k)
-            # Lower limit from idx_low + 1 to idx_upper + 1
-            # Starts from k_i + 1
-            assert limits[k, 1, k_i+1] == np.log2((k_i+1-.5)/k)
-
-
 def DISABLED_test_smooth():
     data = np.empty((1, 1), dtype=np.complex128)
     f_s = 44100
